f2faacd31a0da238809a914557e663e77db45434
tellYoBustas <44.tell.y0@gmail.com>
2023-03-09 14:21:57 +0300

    change scripts

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 const express = require("express")
+const fs = require("fs")
 const app = express()
-const PORT = 3000
+const PORT = 9000
+
+fs.mkdirSync("dist", (error) => {
+    if (error) {
+        console.log("the directory already exists")
+    }
+})
+fs.writeFileSync("dist/_redirects", "/* /index.html 200", (error) => {
+    if (error) {
+        console.log("the file already exists")
+    }
+})
 
 app.use(express.static("dist"))
 
